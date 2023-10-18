4466b50677a15100f1c7b83a31102131a6776c63
Amy Yan <ayanamydev@gmail.com>
2023-10-19 09:50:51 +1100

    feat: editor

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,16 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
   reactStrictMode: true,
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true 
+    return config;
+  },
+
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,10 +1,29 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import '@mdxeditor/editor/style.css';
+import {
+    MDXEditor,
+    headingsPlugin,
+    listsPlugin,
+    quotePlugin,
+    thematicBreakPlugin,
+    UndoRedo,
+    BoldItalicUnderlineToggles,
+    toolbarPlugin,
+} from '@mdxeditor/editor';
+import { useTheme } from "next-themes";
 
 const Post = () => {
     const session = useSession();
     const router = useRouter();
+    const theme = useTheme();
+
+    const [mdxEditorClass, setMdxEditorClass] = useState('');
+
+    useEffect(() => {
+        setMdxEditorClass((theme.resolvedTheme ?? theme.theme) === 'dark' ? 'dark-theme' : '')
+    }, [theme]);
 
     useEffect(() => {
         if (session.status === 'unauthenticated') {
@@ -15,7 +34,23 @@ const Post = () => {
     return (
         <div className="w-full mt-16">
             <div className="max-w-3xl mx-auto p-6">
-                
+                <div className="inline">
+
+                </div>
+                <MDXEditor
+                    className={mdxEditorClass}
+                    markdown='# Hello world'
+                    plugins={[
+                        headingsPlugin(),
+                        listsPlugin(),
+                        quotePlugin(),
+                        thematicBreakPlugin(),
+                        toolbarPlugin({
+                            toolbarContents: () => ( <> <UndoRedo /><BoldItalicUnderlineToggles /></>)
+                        })
+                    ]}
+                    contentEditableClassName="prose dark:prose-invert"
+                />
             </div>
         </div>
     );
