2050933efffbd0b81a8e47b36ba6fb94596b5523
Amy Yan <ayanamydev@gmail.com>
2023-10-19 11:45:37 +1100

    feat: editor

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -36,6 +36,9 @@ import {
 import { useTheme } from "next-themes";
 import { getServerAuthSession } from "~/server/auth";
 import type { GetServerSideProps } from "next";
+import { Input } from "~/components/ui/input";
+import { Textarea } from "~/components/ui/textarea";
+import { Button } from "~/components/ui/button";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const session = await getServerAuthSession(ctx);
@@ -68,87 +71,93 @@ const Post = () => {
 
     return (
         <div className="w-full pt-16 h-screen">
-            <div className="max-w-6xl mx-auto p-6 border-l border-r h-full">
-                <MDXEditor
-                    className={mdxEditorClass}
-                    markdown={markdownContent}
-                    onChange={setMarkdownContent}
-                    placeholder='Start writing...'
-                    plugins={[
-                        toolbarPlugin({
-                            toolbarContents: () => (<>
-                                <DiffSourceToggleWrapper>
-                                    <ConditionalContents
-                                        options={[
-                                        { when: (editor) => editor?.editorType === 'codeblock', contents: () => <ChangeCodeMirrorLanguage /> },
-                                        // { when: (editor) => editor?.editorType === 'sandpack', contents: () => <ShowSandpackInfo /> },
-                                        {
-                                            fallback: () => (
-                                            <>
-                                                <UndoRedo />
-                                                <Separator />
-                                                <BoldItalicUnderlineToggles />
-                                                <CodeToggle />
-                                                <Separator />
-                                                <ListsToggle />
-                                                <Separator />
+            <div className="max-w-6xl mx-auto border-l border-r h-full flex flex-col justify-between py-6">
+                <div className="space-y-3">
+                    <Input placeholder="Enter A Title..." autoFocus className="prose-invert border-none focus-visible:ring-transparent text-4xl" />
+                    <MDXEditor
+                        className={mdxEditorClass}
+                        markdown={markdownContent}
+                        onChange={setMarkdownContent}
+                        placeholder='Start writing...'
+                        plugins={[
+                            toolbarPlugin({
+                                toolbarContents: () => (<>
+                                    <DiffSourceToggleWrapper>
+                                        <ConditionalContents
+                                            options={[
+                                            { when: (editor) => editor?.editorType === 'codeblock', contents: () => <ChangeCodeMirrorLanguage /> },
+                                            // { when: (editor) => editor?.editorType === 'sandpack', contents: () => <ShowSandpackInfo /> },
+                                            {
+                                                fallback: () => (
+                                                <>
+                                                    <UndoRedo />
+                                                    <Separator />
+                                                    <BoldItalicUnderlineToggles />
+                                                    <CodeToggle />
+                                                    <Separator />
+                                                    <ListsToggle />
+                                                    <Separator />
 
-                                                {/* <ConditionalContents
-                                                options={[{ when: whenInAdmonition, contents: () => <ChangeAdmonitionType /> }, { fallback: () => <BlockTypeSelect /> }]}
-                                                /> */}
+                                                    {/* <ConditionalContents
+                                                    options={[{ when: whenInAdmonition, contents: () => <ChangeAdmonitionType /> }, { fallback: () => <BlockTypeSelect /> }]}
+                                                    /> */}
 
-                                                <Separator />
+                                                    <Separator />
 
-                                                <CreateLink />
-                                                <InsertImage />
+                                                    <CreateLink />
+                                                    <InsertImage />
 
-                                                <Separator />
+                                                    <Separator />
 
-                                                <InsertTable />
-                                                <InsertThematicBreak />
+                                                    <InsertTable />
+                                                    <InsertThematicBreak />
 
-                                                <Separator />
-                                                <InsertCodeBlock />
-                                                {/* <InsertSandpack /> */}
+                                                    <Separator />
+                                                    <InsertCodeBlock />
+                                                    {/* <InsertSandpack /> */}
 
-                                                {/* <ConditionalContents
-                                                options={[
-                                                    {
-                                                    when: (editorInFocus) => !whenInAdmonition(editorInFocus),
-                                                    contents: () => (
-                                                        <>
-                                                        <Separator />
-                                                        <InsertAdmonition />
-                                                        </>
-                                                    )
-                                                    }
-                                                ]}
-                                                /> */}
+                                                    {/* <ConditionalContents
+                                                    options={[
+                                                        {
+                                                        when: (editorInFocus) => !whenInAdmonition(editorInFocus),
+                                                        contents: () => (
+                                                            <>
+                                                            <Separator />
+                                                            <InsertAdmonition />
+                                                            </>
+                                                        )
+                                                        }
+                                                    ]}
+                                                    /> */}
 
-                                                <Separator />
-                                            </>
-                                            )
-                                        }
-                                        ]}
-                                    />
-                                    </DiffSourceToggleWrapper>
-                            </>)
-                        }),
-                        listsPlugin(),
-                        quotePlugin(),
-                        headingsPlugin({ allowedHeadingLevels: [1, 2, 3] }),
-                        linkPlugin(),
-                        linkDialogPlugin(),
-                        imagePlugin(),
-                        tablePlugin(),
-                        thematicBreakPlugin(),
-                        codeBlockPlugin({ defaultCodeBlockLanguage: 'txt' }),
-                        codeMirrorPlugin({ codeBlockLanguages: { js: 'JavaScript', css: 'CSS', txt: 'text', tsx: 'TypeScript' } }),
-                        diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown: 'boo' }),
-                        markdownShortcutPlugin(),
-                    ]}
-                    contentEditableClassName="prose dark:prose-invert"
-                />
+                                                    <Separator />
+                                                </>
+                                                )
+                                            }
+                                            ]}
+                                        />
+                                        </DiffSourceToggleWrapper>
+                                </>)
+                            }),
+                            listsPlugin(),
+                            quotePlugin(),
+                            headingsPlugin({ allowedHeadingLevels: [1, 2, 3] }),
+                            linkPlugin(),
+                            linkDialogPlugin(),
+                            imagePlugin(),
+                            tablePlugin(),
+                            thematicBreakPlugin(),
+                            codeBlockPlugin({ defaultCodeBlockLanguage: 'txt' }),
+                            codeMirrorPlugin({ codeBlockLanguages: { js: 'JavaScript', css: 'CSS', txt: 'text', tsx: 'TypeScript' } }),
+                            diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown: 'boo' }),
+                            markdownShortcutPlugin(),
+                        ]}
+                        contentEditableClassName="prose dark:prose-invert"
+                    />
+                </div>
+                <div className="border-t px-6 pt-6 text-right">
+                    <Button>Submit</Button>
+                </div>
             </div>
         </div>
     );
