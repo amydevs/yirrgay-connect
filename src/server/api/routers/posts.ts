0104056128f194f681413e575b7764e42238e1ca
Amy Yan <ayanamydev@gmail.com>
2023-10-19 22:04:08 +1100

    feat: posting

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,11 +1,43 @@
-// import { z } from "zod";
+import { z } from "zod";
 import {
   createTRPCRouter,
-//   protectedProcedure,
+  creatorProtectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
+  create: creatorProtectedProcedure
+    .input(z.object({
+      title: z.string(),
+      content: z.string(),
+    }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.post.create({
+        data: {
+          title: input.title,
+          content: input.content,
+          userId: ctx.session.user.id
+        }
+      });
+    }),
+  update: creatorProtectedProcedure
+    .input(z.object({
+      id: z.string(),
+      title: z.string(),
+      content: z.string(),
+    }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.post.update({
+        where: {
+          id: input.id,
+          userId: ctx.session.user.id,
+        },
+        data: {
+          title: input.title,
+          content: input.content,
+        }
+      });
+    }),
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.post.findMany();
   }),
