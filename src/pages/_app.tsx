cb2429d13a5d9bb6b89c6211b2b6b5fab55c9fd6
Amy Yan <ayanamydev@gmail.com>
2023-10-20 11:43:31 +1100

    feat: inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,40 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { Inter as FontSans } from "next/font/google"
 import { api } from "~/utils/api";
 import ThemeProvider from "~/components/theme-provider";
 import NavBar from "~/components/navbar";
 import "~/styles/globals.css";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <NavBar />
-      <Component {...pageProps} />
-      </ThemeProvider>
+    <>
+      <style jsx global>{`
+				:root {
+					--font-sans: ${fontSans.style.fontFamily};
+				}
+			}`}</style>
+      <SessionProvider session={session}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar />
+          <Component {...pageProps} />
+        </ThemeProvider>
     </SessionProvider>
+    </>
   );
 };
 
